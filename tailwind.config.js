c36a22335ab6bc434137dfbb5c0d5aa7bcc5158b
hansburnthem <hnifrma@gmail.com>
2022-06-11 17:10:12 +0700

    testing public dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.{html,js}'
+    './public/**/*.{html,js}'
   ],
+  darkMode: 'class',
   prefix: 'tw-',
   theme: {
     fontFamily: {
